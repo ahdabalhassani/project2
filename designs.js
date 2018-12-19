5c273d581edb1f50a3ecad3f09466e950148202e
ahdabalhassani <ahdabalhassani@gmail.com>
2018-12-19 19:47:29 +0300

    new updat for file

--- designs.js
+++ designs.js
@@ -1,24 +1,24 @@
 
 
-
+var height, width, col;
 $('#sizePicker').submit(function(event) {
     event.preventDefult();
-    height = $("inputHeight").val();
-    width = $("inputWidth").val();
+    height = $("#inputHeight").val();
+    width = $("#inputWidth").val();
     makeGrid(height, width);
 })
 
 function makeGrid(l, o){
     $("table tr").remove();
     for (var r = 1; r <= l; r++){
-        $("pixelCanvas").append('<tr id = box' + r + '></tr>');
+        $("#pixelCanvas").append('<tr id = box' + r + '></tr>');
         for(var c = 1; c <= o; c++){
             $("#box" + r).append("<td></td>");
         }
     }
 
     $("td").click(function addColor() {
-        col = $("colorPicker").val();
+        col = $("#colorPicker").val();
 
     })
 }
